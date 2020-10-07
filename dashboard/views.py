3f1c5dd7e7ea58f683d3ecdfba9a2c6e86614c97
shudipto <shudipta15-11424@diu.edu.bd>
2020-10-07 21:07:13 +0600

    ourfirstproject

---
+++ dashboard/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+# Create your views here
+
+def dash(request):
+    return render (request, 'dashboard/navbar.html')
+
+def liveR(request):
+    return render (request, 'LiveResult/Live Result.html')
+
+def regcourse(request):
+    return render (request, 'RegCourse/Registered.html')
+
+def teEv(request):
+    return render (request, 'TeachEval/Teaching Evaluation.html')
+
+def drop(request):
+    return render (request, 'DropSeme/drop.html')
+
+def ChangePass(request):
+    return render (request,'ChangePass/Password.html')
+
+  
